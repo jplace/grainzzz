3c068299c7dbcf4ce9e429f15a27962714dcb064
Jordan Place <tapman90@gmail.com>
2021-02-03 19:29:16 -0800

    a color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,32 @@
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        pistachio: [
+          "#f3fae6",
+          "#dbefb3",
+          "#c4e581",
+          "#a0d535",
+          "#88ca02",
+          "#6da202",
+          "#527901",
+          "#293d01",
+          "#0e1400",
+        ],
+        plum: [
+          "#ede6f9",
+          "#cab4ed",
+          "#9568da",
+          "#7136ce",
+          "#4e04c2",
+          "#370388",
+          "#270261",
+          "#17013a",
+          "#080013",
+        ],
+      },
+    },
+  },
   variants: {},
   plugins: [],
 };
