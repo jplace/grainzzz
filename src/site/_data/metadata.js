b5a1041666cdeed72be31543422c20f21ca1f6c5
Jordan Place <tapman90@gmail.com>
2022-12-12 21:39:59 -0800

    absolute url

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "http://localhost:8080", // use this only when absolute URLs are required
+  url: "https://grainzzz.com", // use this only when absolute URLs are required
   defaultDescription: "Instructions on how to cook grains in your rice cooker",
 };
