dc3f8f76d73b5dc0a5134e217dbcb03f52aafeae
Jordan Place <tapman90@gmail.com>
2022-12-12 21:46:51 -0800

    Finesse the description

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,4 +1,5 @@
 module.exports = {
   url: "https://grainzzz.com", // use this only when absolute URLs are required
-  defaultDescription: "Instructions on how to cook grains in your rice cooker",
+  defaultDescription:
+    "Craving grainzzz? Learn how to cook grains in your rice cooker",
 };
