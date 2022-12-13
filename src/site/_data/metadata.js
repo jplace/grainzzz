fe6d05cc7e9ae3a68598d86d94a37142e70c9f8b
Jordan Place <tapman90@gmail.com>
2022-12-12 21:31:06 -0800

    more

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: "http://localhost:8080", // use this only when absolute URLs are required
-  defaultDescription: "Instruction on how to cook grains in your rice cooker",
+  defaultDescription: "Instructions on how to cook grains in your rice cooker",
 };
