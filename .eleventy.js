05fcbc5c5a636abe8ea7dfa098dc79f6ae1cf3f5
Jordan Place <tapman90@gmail.com>
2025-12-03 15:42:22 -0800

    uhhhh trying to upgrade tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -72,20 +72,6 @@ module.exports = function (config) {
     return content;
   });
 
-  // Browsersync to serve 404
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("./dist/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-  });
-
   return {
     dir: {
       input: "src/site",
